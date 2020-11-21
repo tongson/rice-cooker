12b5920d6367be8423d34a93961e84bcebc2f82a
Eduardo Tongson <propolice@gmail.com>
2020-11-21 12:55:21 +0800

    hi: Correct way to pass flags.

--- hi.sh
+++ hi.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env dash
 export LC_ALL=C
-export CFLAGS="-O3 -march=nehalem -mtune=haswell -msse4.2 -pipe -fomit-frame-pointer -ffunction-sections -fdata-sections"
+export CFLAGS="-DNDEBUG -O3 -march=nehalem -mtune=haswell -msse4.2 -pipe -fomit-frame-pointer -ffunction-sections -fdata-sections"
 export LDFLAGS="-Wl,--gc-sections,--strip-all"
 rm -rf hi
 git clone --depth 1 https://github.com/soveran/hi
 cd hi || exit 1
-make -DNDEBUG
+cc -v "$CFLAGS" "$LDFLAGS" hi.c -o hi
 mv hi ../hi.bin
