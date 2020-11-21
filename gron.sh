4b796999140598a71b65fc6d7567ebf7a66ae250
Eduardo Tongson <propolice@gmail.com>
2020-11-21 19:16:26 +0800

    gron: Add standard set -efu and add commit hash to exe filename.

--- gron.sh
+++ gron.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env dash
+set -efu
 rm -rf gron
 git clone --depth 1 https://github.com/tomnomnom/gron
-cd gron || exit 1
-GOOS=linux CGO_ENABLED=0 go build -trimpath -o ../gron.bin -ldflags '-s -w -extldflags "-static"'
+cd gron
+_V=$(git rev-parse --short HEAD)
+GOOS=linux CGO_ENABLED=0 go build -trimpath -o "../gron.${_V}.bin" -ldflags '-s -w -extldflags "-static"'
