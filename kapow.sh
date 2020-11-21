c4fe63e387c84c83f16d1410913c98dba5b9c637
Eduardo Tongson <propolice@gmail.com>
2020-11-21 19:07:03 +0800

    kapow: Standard set -efu.

--- kapow.sh
+++ kapow.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env dash
+set -efu
 rm -rf kapow
 git clone --depth 1 https://github.com/BBVA/kapow
-cd kapow || exit 1
+cd kapow
 GOOS=linux CGO_ENABLED=0 go build -trimpath -o ../kapow.bin -ldflags '-s -w -extldflags "-static"'
