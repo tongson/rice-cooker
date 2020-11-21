67e1f207e2e809cc65563ac8bee95d5fbf94109a
Eduardo Tongson <propolice@gmail.com>
2020-11-21 19:08:12 +0800

    authelia: Standard set -efu.

--- authelia.sh
+++ authelia.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env dash
+set -efu
 rm -rf authelia
 git clone --depth 1 https://github.com/authelia/authelia
-cd authelia || exit 1
+cd authelia
 GOOS=linux CGO_ENABLED=0 go build -trimpath -o ../authelia.bin -ldflags '-s -w -extldflags "-static"' ./cmd/authelia
