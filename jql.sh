6e6bf17a8496c2656fa2e15cb453534535159aab
Eduardo Tongson <propolice@gmail.com>
2020-11-21 19:21:41 +0800

    jql: Add standard set -efu and add commit hash to exe filename.

--- jql.sh
+++ jql.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env dash
+set -efu
 rm -rf jql
 git clone --depth 1 https://github.com/cube2222/jql
-cd jql || exit 1
-GOOS=linux CGO_ENABLED=0 go build -trimpath -o ../jql.bin -ldflags '-s -w -extldflags "-static"'
+cd jql
+_V=$(git rev-parse --short HEAD)
+GOOS=linux CGO_ENABLED=0 go build -trimpath -o "../jql.${_V}.bin" -ldflags '-s -w -extldflags "-static"'
