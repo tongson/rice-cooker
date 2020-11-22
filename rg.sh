90520ee63a9f0422e71439c035dc1314cacda80a
Eduardo Tongson <propolice@gmail.com>
2020-11-22 10:25:13 +0800

    rg: Add commit hash to exe. Move things around.

--- rg.sh
+++ rg.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env dash
+set -efu
 rm -rf rg
 git clone --depth 1 https://github.com/BurntSushi/ripgrep rg
-cd rg || exit 1
+cd rg
+_V=$(git rev-parse --short HEAD)
 cargo build --release
-mv target/release/rg ../rg.bin
-strip -s ../rg.bin
+strip -s target/release/rg
+mv target/release/rg "../rg.${_V}.exe"
