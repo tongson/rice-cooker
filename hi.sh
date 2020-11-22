abb10c4783b3ca81749fcfb7c0b36b482e66b4ae
Eduardo Tongson <propolice@gmail.com>
2020-11-22 10:19:08 +0800

    hi: Not needed.

--- hi.sh
+++ hi.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env dash
 set -efu
 export LC_ALL=C
-export CFLAGS="-DNDEBUG -fPIC -O3 -march=nehalem -mtune=haswell -msse4.2 -pipe -fomit-frame-pointer -ffunction-sections -fdata-sections"
+export CFLAGS="-DNDEBUG -O3 -march=nehalem -mtune=haswell -msse4.2 -pipe -fomit-frame-pointer -ffunction-sections -fdata-sections"
 export LDFLAGS="-Wl,--gc-sections,--strip-all"
 rm -rf hi
 git clone --depth 1 https://github.com/soveran/hi
