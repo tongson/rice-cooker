8f23c9342e10bfffee5d4066d80895e930dec487
Eduardo Tongson <propolice@gmail.com>
2020-11-22 10:00:48 +0800

    authelia: Add commit hash to exe filename.

--- authelia.sh
+++ authelia.sh
@@ -3,4 +3,5 @@ set -efu
 rm -rf authelia
 git clone --depth 1 https://github.com/authelia/authelia
 cd authelia
-GOOS=linux CGO_ENABLED=0 go build -trimpath -o ../authelia.bin -ldflags '-s -w -extldflags "-static"' ./cmd/authelia
+_V=$(git rev-parse --short HEAD)
+GOOS=linux CGO_ENABLED=0 go build -trimpath -o "../authelia.${_V}.exe" -ldflags '-s -w -extldflags "-static"' ./cmd/authelia
