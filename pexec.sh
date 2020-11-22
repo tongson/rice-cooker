cde1e3d87f9f5d140a44a151e36487d47b2acb34
Eduardo Tongson <propolice@gmail.com>
2020-11-22 12:38:03 +0800

    pexec: Use custom compilation flags.

--- pexec.sh
+++ pexec.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env dash
-rm -rf pipe_exec
-git clone --depth 1 https://github.com/koraa/pipe_exec
-cd pipe_exec || exit 1
-make
-mv pexec ../pexec.bin
+set -efu
+export LC_ALL=C
+export CFLAGS="-D_GNU_SOURCE -DNDEBUG -O3 -march=nehalem -mtune=haswell -msse4.2 -pipe -fomit-frame-pointer -ffunction-sections -fdata-sections"
+export LDFLAGS="-Wl,--gc-sections,--strip-all"
+rm -rf pexec
+git clone --depth 1 https://github.com/koraa/pipe_exec pexec
+cd pexec
+_V=$(git rev-parse --short HEAD)
+cc -v -std=c11 "$CFLAGS" "$LDFLAGS" pexec.c -o "../pexec.${_V}.exe"
