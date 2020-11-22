e9862670f66288e7dd8b01d84fe56480f465039e
Eduardo Tongson <propolice@gmail.com>
2020-11-22 10:01:01 +0800

    kapow: Add commit hash to exe filename.

--- kapow.sh
+++ kapow.sh
@@ -3,4 +3,5 @@ set -efu
 rm -rf kapow
 git clone --depth 1 https://github.com/BBVA/kapow
 cd kapow
-GOOS=linux CGO_ENABLED=0 go build -trimpath -o ../kapow.bin -ldflags '-s -w -extldflags "-static"'
+_V=$(git rev-parse --short HEAD)
+GOOS=linux CGO_ENABLED=0 go build -trimpath -o "../kapow.${_V}.exe" -ldflags '-s -w -extldflags "-static"'
