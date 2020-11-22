42e9e3287bbc61f2a8ad67c6ac48acbad25dd8d2
Eduardo Tongson <propolice@gmail.com>
2020-11-22 10:05:11 +0800

    map: Use compile flags and add commit hash to exe.

--- map.sh
+++ map.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env dash
+set -efu
+export LC_ALL=C
+export CFLAGS="-DNDEBUG -fPIC -O3 -march=nehalem -mtune=haswell -msse4.2 -pipe -fomit-frame-pointer -ffunction-sections -fdata-sections"
+export LDFLAGS="-Wl,--gc-sections,--strip-all"
 rm -rf map
 git clone --depth 1 https://github.com/soveran/map
-cd map || exit 1
-make
-mv map ../map.bin
+cd map
+_V=$(git rev-parse --short HEAD)
+cc -v "$CFLAGS" "$LDFLAGS" map.c -o "../map.${_V}.exe"
