c75bdf20b739570170abcb496864bff6968dd2c9
Eduardo Tongson <propolice@gmail.com>
2020-11-22 10:01:21 +0800

    rr: Add commit hash to exe filename.

--- rr.sh
+++ rr.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env dash
+set -efu
 rm -rf rr
 git clone --depth 1 https://github.com/tongson/rr
-cd rr || exit 1
-GOOS=linux CGO_ENABLED=0 go build -trimpath -o ../rr.bin -ldflags '-s -w -extldflags "-static"' ./cmd/rr
+cd rr
+_V=$(git rev-parse --short HEAD)
+GOOS=linux CGO_ENABLED=0 go build -trimpath -o "../rr.${_V}.exe" -ldflags '-s -w -extldflags "-static"' ./cmd/rr
