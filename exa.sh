2a9050269f7ff6f4b16a2dde65fc313bb257ec1c
Eduardo Tongson <propolice@gmail.com>
2020-11-22 13:34:35 +0800

    exa: Should use exe extension.

--- exa.sh
+++ exa.sh
@@ -5,5 +5,5 @@ git clone --depth 1 https://github.com/ogham/exa
 cd exa
 _V=$(git rev-parse --short HEAD)
 cargo build --release
-mv target/release/exa "../exa.${_V}.bin"
-strip -s "../exa.${_V}.bin"
+mv target/release/exa "../exa.${_V}.exe"
+strip -s "../exa.${_V}.exe"
