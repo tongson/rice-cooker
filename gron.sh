8c4b7273274ec8af135d6113abb922c66660cd56
Eduardo Tongson <propolice@gmail.com>
2020-11-22 13:35:03 +0800

    gron: Should use exe extension.

--- gron.sh
+++ gron.sh
@@ -4,4 +4,4 @@ rm -rf gron
 git clone --depth 1 https://github.com/tomnomnom/gron
 cd gron
 _V=$(git rev-parse --short HEAD)
-GOOS=linux CGO_ENABLED=0 go build -trimpath -o "../gron.${_V}.bin" -ldflags '-s -w -extldflags "-static"'
+GOOS=linux CGO_ENABLED=0 go build -trimpath -o "../gron.${_V}.exe" -ldflags '-s -w -extldflags "-static"'
