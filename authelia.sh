30b738bb49f1130d9d2c68f043765a74f548f363
Eduardo Tongson <propolice@gmail.com>
2020-12-25 15:06:29 +0800

    authelia: Use a tagged version.

--- authelia.sh
+++ authelia.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env dash
 set -efu
 rm -rf authelia
-git clone --depth 1 https://github.com/authelia/authelia
+_V="v4.24.1"
+git clone --depth 1 --branch "${_V}" https://github.com/authelia/authelia
 cd authelia
-_V=$(git rev-parse --short HEAD)
 GOOS=linux CGO_ENABLED=0 go build -trimpath -o "../authelia.${_V}.exe" -ldflags '-s -w -extldflags "-static"' ./cmd/authelia
