0c25a1294fec793b0e9402b36f1e6bf5d19b67f5
Eduardo Tongson <ed@0srv.com>
2021-03-01 20:18:22 +0800

    gopls: Fix filename.

--- gopls.sh
+++ gopls.sh
@@ -5,4 +5,4 @@ _V="gopls/v0.6.5"
 git clone --depth 1 --branch "${_V}" https://github.com/golang/tools gopls
 cd gopls/gopls
 GOOS=linux CGO_ENABLED=0 go build \
-  -trimpath -o "../..//gopls.${_V}.exe" -ldflags '-s -w -extldflags "-static"'
+  -trimpath -o "../../gopls.exe" -ldflags '-s -w -extldflags "-static"'
