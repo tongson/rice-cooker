110eb64784fbca753002801eb854d302e35d4886
Eduardo Tongson <ed@0srv.com>
2021-03-02 22:04:17 +0800

    lib: Determine short tag.

--- .lib/002-go.sh
+++ .lib/002-go.sh
@@ -1,8 +1,9 @@
 _go_static_build()
 {
   cd "${NAME}"
+  local tag="${TAG:-$(git rev-parse --short HEAD)}"
   GOOS=linux CGO_ENABLED=0 go build \
     -trimpath -ldflags '-s -w -extldflags "-static"' \
-    -o "${_TARGET}/${NAME}.${TAG}" "${@}"
+    -o "${_TARGET}/${NAME}.${tag}" "${@}"
 }
 
