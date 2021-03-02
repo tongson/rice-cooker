f371073f496539d15a1eb0834dd3022e17621dce
Eduardo Tongson <ed@0srv.com>
2021-03-02 20:02:28 +0800

    rr: Migrate to rr.

--- rr.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env dash
-set -efu
-rm -rf rr
-git clone --depth 1 https://github.com/tongson/rr
-cd rr
-_V=$(git rev-parse --short HEAD)
-GOOS=linux CGO_ENABLED=0 go build -trimpath -o "../rr.${_V}.exe" -ldflags '-s -w -extldflags "-static"' ./cmd/rr
