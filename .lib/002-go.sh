ad5641d2966194a093db7457d1cbd39321120e2c
Eduardo Tongson <propolice@gmail.com>
2021-03-03 01:07:47 +0800

    lib: Allow setting name of executable.

--- .lib/002-go.sh
+++ .lib/002-go.sh
@@ -2,8 +2,9 @@ _go_static_build()
 {
   cd "${NAME}"
   local tag="${TAG:-$(git rev-parse --short HEAD)}"
+  local bin="${2:-${NAME}}"
   GOOS=linux CGO_ENABLED=0 go build \
     -trimpath -ldflags '-s -w -extldflags "-static"' \
-    -o "${_TARGET}/${NAME}.${tag}" "${@}"
+    -o "${_TARGET}/${bin}.${tag}" "${1}" 
+  cd -
 }
-
