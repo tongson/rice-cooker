e2f8536f4bbaa888481f2d0372132421d1510bf0
Eduardo Tongson <ed@0srv.com>
2021-03-03 00:58:21 +0800

    lib: Rename and new function.

--- .lib/003-git.sh
+++ .lib/003-git.sh
@@ -3,7 +3,12 @@ _git_clone_tag()
   git clone --depth 1 --branch "${TAG}" "${SRC}" "${NAME}"
 }
 
-_git_clone()
+_git_clone_src()
 {
   git clone --depth 1 "${SRC}" "${NAME}"
+}
+
+_git_clone()
+{
+  git clone --depth 1 "${1}"
 }
