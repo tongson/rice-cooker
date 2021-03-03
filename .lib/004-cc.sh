deb2bfc8bca93df1fb44724199f7f40a5cf4633f
Eduardo Tongson <propolice@gmail.com>
2021-03-03 19:50:55 +0800

    lib: ShellCheck suggestions.

--- .lib/004-cc.sh
+++ .lib/004-cc.sh
@@ -1,8 +1,7 @@
 _cc()
-{
-  cd "${NAME}"
+(
+  cd "${NAME}" || exit
   local tag="${TAG:-$(git rev-parse --short HEAD)}"
   local bin="${2:-${NAME}}"
   cc -v "${CFLAGS}" "${LDFLAGS}" -o "${_TARGET}/${bin}.${tag}" "${1}" 
-  cd -
-}
+)
