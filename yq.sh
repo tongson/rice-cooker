5142cf541a5b7534494a5e9f4ed2803464707355
Eduardo Tongson <propolice@gmail.com>
2021-03-03 19:56:13 +0800

    yq: Convert to rr modulde.

--- yq.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env dash
-set -efu
-rm -rf yq
-git clone --depth 1 https://github.com/mikefarah/yq
-cd yq
-_V=$(git rev-parse --short HEAD)
-GOOS=linux CGO_ENABLED=0 go build -trimpath -o "../yq.${_V}.exe" -ldflags '-s -w -extldflags "-static"' yq.go
