74f5a2509d732d000be9c02266e8ae3fa4b0caa5
Eduardo Tongson <propolice@gmail.com>
2021-03-03 21:40:24 +0800

    rg: Convert to rr module.

--- rg.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env dash
-set -efu
-rm -rf rg
-git clone --depth 1 https://github.com/BurntSushi/ripgrep rg
-cd rg
-_V=$(git rev-parse --short HEAD)
-cargo build --release
-strip -s target/release/rg
-mv target/release/rg "../rg.${_V}.exe"
