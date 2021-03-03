be548de0ab4afdf56d1e880c7f829e3c08eb7c93
Eduardo Tongson <propolice@gmail.com>
2021-03-03 18:37:22 +0800

    age: Already an rr module.

--- age.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env dash
-set -efu
-rm -rf age
-git clone --depth 1 https://github.com/FiloSottile/age
-cd age
-_V=$(git rev-parse --short HEAD)
-GOOS=linux CGO_ENABLED=0 go build -trimpath -o "../age.${_V}.exe" -ldflags '-s -w -extldflags "-static"' ./cmd/age
-GOOS=linux CGO_ENABLED=0 go build -trimpath -o "../age-keygen.${_V}.exe" -ldflags '-s -w -extldflags "-static"' ./cmd/age-keygen
