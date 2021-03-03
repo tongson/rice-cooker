8c94a490cfee282fe44cb8771775ca41ee41139d
Eduardo Tongson <propolice@gmail.com>
2021-03-03 18:59:02 +0800

    chore: Already converted.

--- traefik.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env dash
-set -efu
-rm -rf traefik
-_V="v2.3.5"
-git clone --depth 1 --branch "${_V}" https://github.com/traefik/traefik
-cd traefik
-go generate
-GOOS=linux GOGC=off CGO_ENABLED=0 go build \
-  -trimpath -o "../traefik.${_V}.exe" -ldflags '-s -w -extldflags "-static"' ./cmd/traefik
