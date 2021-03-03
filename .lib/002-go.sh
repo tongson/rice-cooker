9a207307ca65503121686c6a58ab752caaacd03b
Eduardo Tongson <propolice@gmail.com>
2021-03-03 19:48:31 +0800

    lib: ShellCheck suggestions.

--- .lib/002-go.sh
+++ .lib/002-go.sh
@@ -1,17 +1,15 @@
 _go_static_build()
-{
-  cd "${NAME}"
+(
+  cd "${NAME}" || exit
   local tag="${TAG:-$(git rev-parse --short HEAD)}"
   local bin="${2:-${NAME}}"
   GOOS=linux CGO_ENABLED=0 go build \
     -trimpath -ldflags '-s -w -extldflags "-static"' \
     -o "${_TARGET}/${bin}.${tag}" "${1}" 
-  cd -
-}
+)
 
 _go_generate()
-{
-  cd "${NAME}"
+(
+  cd "${NAME}" || exit
   go generate
-  cd -
-}
+)
