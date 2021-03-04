6423992c1d4bd1e345675546ddd9d1003e8708b4
Eduardo Tongson <propolice@gmail.com>
2021-03-04 09:14:13 +0800

    lib: Default flags for C compilation.

--- .lib/004-cc.sh
+++ .lib/004-cc.sh
@@ -3,5 +3,7 @@ _cc()
   cd "${NAME}" || exit
   local tag="${TAG:-$(git rev-parse --short HEAD)}"
   local bin="${2:-${NAME}}"
-  cc -v "${CFLAGS}" "${LDFLAGS}" -o "${_TARGET}/${bin}.${tag}" "${1}" 
+  local cflags="${CFLAGS:--Os -pipe -fomit-frame-pointer -march=nocona}"
+  local ldflags="${LDFLAGS:--Wl,--strip-all}"
+  cc -v "${cflags}" "${ldflags}" -o "${_TARGET}/${bin}.${tag}" "${1}"
 )
