f525514e6a555763457b77c1227aa71d35a8a85f
Eduardo Tongson <propolice@gmail.com>
2021-03-04 09:36:08 +0800

    terraform: Convert to rr module.

--- terraform.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env dash
-set -efu
-rm -rf terraform
-_V="v0.14.7"
-git clone --depth 1 --branch "${_V}" https://github.com/hashicorp/terraform
-cd terraform
-GOOS=linux CGO_ENABLED=0 go build \
-  -trimpath -o "../terraform.${_V}.exe" -ldflags '-s -w -extldflags "-static"'
