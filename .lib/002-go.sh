9f4cbfccf8c7cf34cb9022f176848c248fd9886a
Eduardo Tongson <propolice@gmail.com>
2021-03-04 09:35:55 +0800

    lib: Default for go build is CWD.

--- .lib/002-go.sh
+++ .lib/002-go.sh
@@ -3,9 +3,10 @@ _go_build()
   cd "${NAME}" || exit
   local tag="${TAG:-$(git rev-parse --short HEAD)}"
   local bin="${2:-${NAME}}"
+  local x="${1:-.}"
   GOOS=linux CGO_ENABLED=0 go build \
     -trimpath -ldflags '-s -w -extldflags "-static"' \
-    -o "${_TARGET}/${bin}.${tag}" "${1}" 
+    -o "${_TARGET}/${bin}.${tag}" "${x}"
 )
 
 _go_generate()
