b0c230559f0dbd6989dddae5a6ff0b483c3eb683
Eduardo Tongson <propolice@gmail.com>
2021-03-04 08:38:00 +0800

    lib: Rename cloning functions.

--- .lib/003-git.sh
+++ .lib/003-git.sh
@@ -3,12 +3,12 @@ _git_clone_tag()
   git clone --depth 1 --branch "${TAG}" "${SRC}" "${NAME}"
 }
 
-_git_clone_src()
+_git_clone()
 {
   git clone --depth 1 "${SRC}" "${NAME}"
 }
 
-_git_clone()
+_git_clone_url()
 {
   git clone --depth 1 "${1}"
 }
