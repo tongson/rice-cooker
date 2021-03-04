55b4ae30cef5f494f67e4f75c3352c5fb6bdd6de
Eduardo Tongson <propolice@gmail.com>
2021-03-04 09:16:01 +0800

    lib: Declare NDEBUG for cc.

--- .lib/004-cc.sh
+++ .lib/004-cc.sh
@@ -3,7 +3,7 @@ _cc()
   cd "${NAME}" || exit
   local tag="${TAG:-$(git rev-parse --short HEAD)}"
   local bin="${2:-${NAME}}"
-  local cflags="${CFLAGS:--Os -pipe -fomit-frame-pointer -march=nocona}"
+  local cflags="${CFLAGS:--DNDEBUG -Os -pipe -fomit-frame-pointer -march=nocona}"
   local ldflags="${LDFLAGS:--Wl,--strip-all}"
   cc -v "${cflags}" "${ldflags}" -o "${_TARGET}/${bin}.${tag}" "${1}"
 )
