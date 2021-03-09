c42d73d5e050055bad305b536a9b486fa2d40652
Eduardo Tongson <propolice@gmail.com>
2021-03-09 21:16:01 +0800

    selene: Convert to rr module.

--- selene.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env dash
-set -efu
-rm -rf selene
-_V="0.11.0"
-git clone --depth 1 --branch "${_V}" https://github.com/Kampfkarren/selene
-cd selene
-cargo build --release
-strip -s target/release/selene
-mv target/release/selene "../selene.${_V}.exe"
