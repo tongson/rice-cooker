58c6eefa18df2e30300aff70e41ec786550644d8
Eduardo Tongson <propolice@gmail.com>
2021-03-09 20:56:54 +0800

    kapow: Convert to rr module.

--- kapow.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env dash
-set -efu
-rm -rf kapow
-git clone --depth 1 https://github.com/BBVA/kapow
-cd kapow
-_V=$(git rev-parse --short HEAD)
-GOOS=linux CGO_ENABLED=0 go build -trimpath -o "../kapow.${_V}.exe" -ldflags '-s -w -extldflags "-static"'
