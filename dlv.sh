2506a15af767e76589d2ed61869039aa8b4f821e
Eduardo Tongson <propolice@gmail.com>
2021-03-10 17:46:26 +0800

    delve: Convert to rr module.

--- dlv.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env dash
-set -efu
-rm -rf dlv
-_V="v1.6.0"
-git clone --depth 1 --branch "${_V}" https://github.com/go-delve/delve dlv
-cd dlv
-GOOS=linux CGO_ENABLED=0 go build \
-  -trimpath -o "../dlv.${_V}.exe" -ldflags '-s -w -extldflags "-static"' ./cmd/dlv
