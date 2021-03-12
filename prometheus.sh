d12334bf1efb7c5ce3bfbf1f584f4940389cbc47
Eduardo Tongson <propolice@gmail.com>
2021-03-12 23:30:45 +0800

    prometheus: Convert to rr module.

--- prometheus.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env dash
-set -efu
-rm -rf prometheus
-_V="v2.23.0"
-git clone --depth 1 --branch "${_V}" https://github.com/prometheus/prometheus
-cd prometheus
-GOOS=linux CGO_ENABLED=0 go build \
-  -trimpath -o "../prometheus.${_V}.exe" -ldflags '-s -w -extldflags "-static"' ./cmd/prometheus
