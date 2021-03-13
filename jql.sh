5d112a7e4c5dea115781d7e8e9ad09fc76da9458
Eduardo Tongson <propolice@gmail.com>
2021-03-13 17:44:27 +0800

    jql: Convert to rr module.

--- jql.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env dash
-set -efu
-rm -rf jql
-git clone --depth 1 https://github.com/cube2222/jql
-cd jql
-_V=$(git rev-parse --short HEAD)
-GOOS=linux CGO_ENABLED=0 go build -trimpath -o "../jql.${_V}.exe" -ldflags '-s -w -extldflags "-static"'
