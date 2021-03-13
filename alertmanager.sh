d1fe1434b7630147da6e9d0447a9d3972234b19f
Eduardo Tongson <propolice@gmail.com>
2021-03-13 17:20:41 +0800

    alertmanager: Convert to rr module.

--- alertmanager.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env dash
-set -efu
-rm -rf alertmanager
-_V="v0.21.0"
-git clone --depth 1 --branch "${_V}" https://github.com/prometheus/alertmanager
-cd alertmanager
-GOOS=linux CGO_ENABLED=0 go build \
-  -trimpath -o "../alertmanager.${_V}.exe" -ldflags '-s -w -extldflags "-static"' ./cmd/alertmanager
