bf949fdaeaa6f5144d9e35cdabbd72f982d8fdb9
Eduardo Tongson <propolice@gmail.com>
2021-03-13 17:51:04 +0800

    smtp_to_telegram: Convert to rr module.

--- smtp_to_telegram.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env dash
-set -efu
-rm -rf smtp_to_telegram
-git clone --depth 1 https://github.com/KostyaEsmukov/smtp_to_telegram
-cd smtp_to_telegram
-_V=$(git rev-parse --short HEAD)
-GOOS=linux CGO_ENABLED=0 go build -trimpath -o "../smtp_to_telegram.${_V}.exe" -ldflags '-s -w -extldflags "-static"'
