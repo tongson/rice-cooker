def16c687c8ed030bd92405f3fbb47b872111a37
Eduardo Tongson <propolice@gmail.com>
2021-03-13 13:41:51 +0800

    blackbox_exporter: Convert to rr module.

--- blackbox_exporter.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env dash
-set -efu
-rm -rf blackbox_exporter
-_V="v0.18.0"
-git clone --depth 1 --branch "${_V}" https://github.com/prometheus/blackbox_exporter
-cd blackbox_exporter
-GOOS=linux CGO_ENABLED=0 go build \
-  -trimpath -o "../blackbox_exporter.${_V}.exe" -ldflags '-s -w -extldflags "-static"'
