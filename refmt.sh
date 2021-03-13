12452f9eaaabb7783df305877250d292d4b4e0c8
Eduardo Tongson <propolice@gmail.com>
2021-03-13 09:02:19 +0800

    refmt: Convert to rr module.

--- refmt.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env dash
-set -efu
-rm -rf refmt
-git clone --depth 1 https://github.com/yoshihitoh/refmt
-cd refmt
-_V=$(git rev-parse --short HEAD)
-cargo build --release
-strip -s target/release/refmt
-mv target/release/refmt "../refmt.${_V}.exe"
