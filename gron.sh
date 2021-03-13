15e10d83d6f5fecbbe425aeb5299b10806c51e29
Eduardo Tongson <propolice@gmail.com>
2021-03-13 08:49:51 +0800

    gron: Convert to rr module.

--- gron.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env dash
-set -efu
-rm -rf gron
-git clone --depth 1 https://github.com/tomnomnom/gron
-cd gron
-_V=$(git rev-parse --short HEAD)
-GOOS=linux CGO_ENABLED=0 go build -trimpath -o "../gron.${_V}.exe" -ldflags '-s -w -extldflags "-static"'
