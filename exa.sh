55360f729c41c88abc63098f3e6b06b23263ca2b
Eduardo Tongson <propolice@gmail.com>
2021-03-13 08:53:28 +0800

    exa: Convert to rr module.

--- exa.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env dash
-set -efu
-rm -rf exa
-git clone --depth 1 https://github.com/ogham/exa
-cd exa
-_V=$(git rev-parse --short HEAD)
-cargo build --release
-mv target/release/exa "../exa.${_V}.exe"
-strip -s "../exa.${_V}.exe"
