5100e297b74716860c34dad2d18cfb378d0032ba
Eduardo Tongson <propolice@gmail.com>
2021-03-13 17:40:51 +0800

    authelia: Convert to rr module.

--- authelia.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env dash
-set -efu
-rm -rf authelia
-_V="v4.24.1"
-git clone --depth 1 --branch "${_V}" https://github.com/authelia/authelia
-cd authelia
-GOOS=linux CGO_ENABLED=0 go build -trimpath -o "../authelia.${_V}.exe" -ldflags '-s -w -extldflags "-static"' ./cmd/authelia
