96d5697a5fb22a462e47ab23db7b554cd281d31f
Eduardo Tongson <propolice@gmail.com>
2021-03-13 17:35:06 +0800

    node_exporter: Convert to rr module.

--- node_exporter.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env dash
-set -efu
-rm -rf node_exporter
-_V="v1.0.1"
-git clone --depth 1 --branch "${_V}" https://github.com/prometheus/node_exporter
-cd node_exporter
-GOOS=linux CGO_ENABLED=0 go build \
-  -trimpath -o "../node_exporter.${_V}.exe" -ldflags '-s -w -extldflags "-static"'
