36154d456595582e85fb0443a7ff91404a9a7b16
Eduardo Tongson <propolice@gmail.com>
2021-03-13 17:32:09 +0800

    redwood: Went with another solution.

--- redwood.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env dash
-set -efu
-rm -rf redwood
-git clone --depth 1 https://github.com/andybalholm/redwood
-cd redwood
-_V=$(git rev-parse --short HEAD)
-GOOS=linux CGO_ENABLED=0 go build -trimpath -o "../redwood.${_V}.exe" -ldflags '-s -w -extldflags "-static"'
