879be180a6539758262277f294c770f113a1d48d
Eduardo Tongson <propolice@gmail.com>
2021-03-13 17:53:41 +0800

    No need for these right now.

--- gopkgs.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env dash
-set -efu
-rm -rf gopkgs
-_V="v2.1.2"
-git clone --depth 1 --branch "${_V}" https://github.com/uudashr/gopkgs
-cd gopkgs
-GOOS=linux CGO_ENABLED=0 go build \
-  -trimpath -o "../gopkgs.${_V}.exe" -ldflags '-s -w -extldflags "-static"' ./cmd/gopkgs
--- gopls.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env dash
-set -efu
-rm -rf gopls
-_V="gopls/v0.6.5"
-git clone --depth 1 --branch "${_V}" https://github.com/golang/tools gopls
-cd gopls/gopls
-GOOS=linux CGO_ENABLED=0 go build \
-  -trimpath -o "../../gopls.exe" -ldflags '-s -w -extldflags "-static"'
