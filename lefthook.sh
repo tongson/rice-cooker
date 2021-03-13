66e6be9b0153d1000fe315d88aeb484c53624be1
Eduardo Tongson <propolice@gmail.com>
2021-03-13 16:58:37 +0800

    lefthook: Convert to rr module.

--- lefthook.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env dash
-set -efu
-rm -rf lefthook
-git clone --depth 1 https://github.com/Arkweid/lefthook
-cd lefthook 
-_V=$(git rev-parse --short HEAD)
-GOOS=linux CGO_ENABLED=0 go build -trimpath -o "../lefthook.${_V}.exe" -ldflags '-s -w -extldflags "-static"'
