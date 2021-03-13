ef2ecf1c37f850029df63e74437f2fb0dc4ef20f
Eduardo Tongson <propolice@gmail.com>
2021-03-13 17:52:23 +0800

    unbound_exporter: Convert to rr module.

--- unbound_exporter.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env dash
-set -efu
-rm -rf unbound_exporter
-git clone --depth 1 https://github.com/kumina/unbound_exporter
-cd unbound_exporter
-_V=$(git rev-parse --short HEAD)
-GOOS=linux CGO_ENABLED=0 go build -trimpath -o "../unbound_exporter.${_V}.exe" -ldflags '-s -w -extldflags "-static"'
