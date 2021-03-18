d8e93051101b23ef015e2ca21d4627907ddaf242
Eduardo Tongson <propolice@gmail.com>
2021-03-18 19:54:23 +0800

    lib: Check if git is installed.

--- .lib/002-git.sh
+++ .lib/002-git.sh
@@ -1,19 +1,28 @@
+_git_prelude()
+{
+  command -v git || { printf "git command not found.\\n"; exit 1; }
+}
+
 _git_clone_tag()
 {
+  _git_prelude
   git clone --depth 1 --branch "${TAG}" "${SRC}" "${NAME}"
 }
 
 _git_clone()
 {
+  _git_prelude
   git clone --depth 1 "${SRC}" "${NAME}"
 }
 
 _git_clone_url()
 {
+  _git_prelude
   git clone --depth 1 "${1}"
 }
 
 _git_head()
 {
+  _git_prelude
   git rev-parse --short HEAD
 }
