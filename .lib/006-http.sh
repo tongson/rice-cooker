7288c6621b7ba3b3580ed59a8a0ed94589928438
Eduardo Tongson <propolice@gmail.com>
2021-03-18 20:04:33 +0800

    lib: Check if these commands are availab.e

--- .lib/006-http.sh
+++ .lib/006-http.sh
@@ -1,11 +1,13 @@
 _get()
 (
+  command -v curl || { printf "curl command not found.\\n"; exit 1; }
   cd "${_SRC}" || exit 1
   test -f "${_SRC}/${NAME}.zip" || curl -LO "${SRC}"
 )
 
 _unzip()
 (
+  command -v unzip || { printf "unzip command not found.\\n"; exit 1; }
   cd "${_WORK}" || exit 1
   unzip "${_SRC}/${NAME}.zip"
 )
