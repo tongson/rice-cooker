3e3bdb51c038dba91d83945f3fc1ef984aae75d7
Eduardo Tongson <propolice@gmail.com>
2021-03-29 15:44:10 +0800

    lib: Mark start of git operations.

--- .lib/002-git.sh
+++ .lib/002-git.sh
@@ -1,23 +1,26 @@
 _git_prelude()
 {
-  command -v git || { printf "git command not found.\\n"; exit 1; }
+  command -v git >&- || { printf "git command not found.\\n"; exit 1; }
 }
 
 _git_clone_tag()
 {
   _git_prelude
+  __mark "git clone ${SRC}:${TAG}"
   git clone --depth 1 --branch "${TAG}" "${SRC}" "${NAME}"
 }
 
 _git_clone()
 {
   _git_prelude
+  __mark "git clone ${SRC}"
   git clone --depth 1 "${SRC}" "${NAME}"
 }
 
 _git_clone_url()
 {
   _git_prelude
+  __mark "git clone ${1}"
   git clone --depth 1 "${1}"
 }
 
