5fa5ce9468c022ed4e77776bdafc53546dfbccd8
Eduardo Tongson <propolice@gmail.com>
2021-03-29 15:44:52 +0800

    lib: Mark start of cargo build. Silence checking.

--- .lib/005-cargo.sh
+++ .lib/005-cargo.sh
@@ -1,9 +1,10 @@
 _cargo_build()
 (
-  command -v cargo || { printf "cargo command not found.\\n"; exit 1; }
+  command -v cargo >&- || { printf "cargo command not found.\\n"; exit 1; }
   cd "${NAME}" || exit
   local tag="${TAG:-$(git rev-parse --short HEAD)}"
   local bin="${2:-${NAME}}"
+  __mark "cargo build ${bin}"
   cargo build --release
   strip -s "target/release/${NAME}"
   mv "target/release/${NAME}" "${_TARGET}/${bin}.${tag}"
