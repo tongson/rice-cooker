ef5bd226f1e1eb7ba74127f7cf9b0c09f55b05b4
Eduardo Tongson <propolice@gmail.com>
2021-06-22 19:24:07 +0800

    lib: For building Intel macOS binaries.

--- .lib/003-go.sh
+++ .lib/003-go.sh
@@ -16,6 +16,19 @@ _go_build()
     -o "${_TARGET}/${bin}.${tag}" "${x}"
 )
 
+_go_build_macos()
+(
+  _go_prelude
+  cd "${NAME}" || exit 1
+  local tag="${TAG:-$(git rev-parse --short HEAD)}"
+  local bin="${2:-${NAME}}"
+  local x="${1:-.}"
+  __mark "go build ${bin}"
+  GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build \
+    -trimpath -ldflags '-s -w' \
+    -o "${_TARGET}/${bin}.${tag}" "${x}"
+)
+
 _cgo_build()
 (
   _go_prelude
