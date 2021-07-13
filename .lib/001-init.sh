3e966e9bf0a2ffc8719a3314505bb9e6a2ce2440
Eduardo Tongson <propolice@gmail.com>
2021-07-13 13:33:05 +0800

    lib: Check for errors,

--- .lib/001-init.sh
+++ .lib/001-init.sh
@@ -5,15 +5,19 @@ mkdir -p "${_SRC}"
 _WORK=$(mktemp -d)
 _trap()
 {
+  local exit_status=${1:-$?}
+  if [ $exit_status = 0 ]
+  then
+    printf "\nCleaning up.\n"
+  else
+    printf 1>&2 "\nSomething when wrong.\n"
+  fi
   rm -rf "${_WORK}"
-  printf 1>&2 "\nSomething went wrong or we were interrupted.\n"
   trap - EXIT
   exit
 }
-
 trap _trap EXIT INT HUP
 cd "${_WORK}" || exit 1
-
 _chdir_build()
 {
   cd "${NAME}" || exit 1
