8ade665212bd01085f29d24f489ceb9f8131bcba
Eduardo Tongson <propolice@gmail.com>
2021-07-13 13:08:09 +0800

    lib: Use a shell function so we can add some notifications.

--- .lib/001-init.sh
+++ .lib/001-init.sh
@@ -3,7 +3,15 @@ mkdir -p "${_TARGET}"
 _SRC="${HOME}/src"
 mkdir -p "${_SRC}"
 _WORK=$(mktemp -d)
-trap 'rm -rf "${_WORK}"; trap - EXIT; exit' EXIT INT HUP
+_trap()
+{
+  rm -rf "${_WORK}"
+  printf "Something went wrong or we were interrupted.\n"
+  trap - EXIT
+  exit
+}
+
+trap _trap EXIT INT HUP
 cd "${_WORK}" || exit 1
 
 _chdir_build()
