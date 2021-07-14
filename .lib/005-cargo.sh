caf37e40774dbcc634ac2970968d7c80378dfd72
Eduardo Tongson <propolice@gmail.com>
2021-07-14 19:13:34 +0800

    lib: Should just be $NAME.

--- .lib/005-cargo.sh
+++ .lib/005-cargo.sh
@@ -3,9 +3,8 @@ _cargo_build()
   command -v cargo >/dev/null || { printf 1>&2 "cargo command not found.\\n"; exit 1; }
   cd "${NAME}" || exit
   local tag="${TAG:-$(git rev-parse --short HEAD)}"
-  local bin="${2:-${NAME}}"
-  __mark "cargo build ${bin}"
+  __mark "cargo build ${NAME}"
   cargo build --release --locked "$@"
   strip -s "target/release/${NAME}"
-  mv "target/release/${NAME}" "${_TARGET}/${bin}.${tag}"
+  mv "target/release/${NAME}" "${_TARGET}/${NAME}.${tag}"
 )
