da36a6bbd0400bd2305611824127fc65e541e67f
Eduardo Tongson <propolice@gmail.com>
2021-07-24 13:33:59 +0800

    lib: Symlink to new executable.

--- .lib/005-cargo.sh
+++ .lib/005-cargo.sh
@@ -3,8 +3,10 @@ _cargo_build()
   command -v cargo >/dev/null || { printf 1>&2 "cargo command not found.\\n"; exit 1; }
   cd "${NAME}" || exit
   local tag="${TAG:-$(git rev-parse --short HEAD)}"
+  local exe="${NAME}.${tag}"
   __mark "cargo build ${NAME}"
   cargo build --release --locked "$@"
   strip -s "target/release/${NAME}"
-  mv "target/release/${NAME}" "${_TARGET}/${NAME}.${tag}"
+  mv "target/release/${NAME}" "${_TARGET}/${exe}"
+  ln -sf "${_TARGET}/${exe}" "${_TARGET}/${NAME}"
 )
